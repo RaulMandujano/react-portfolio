b9666d3eefee2d43106902ec5505ff97c785accc
Raul Mandujano <raulmandujano@Rauls-Mac-mini.local>
2023-02-08 22:52:45 -0700

    portfolio

---
+++ src/content_option.js
@@ -0,0 +1,172 @@
+const logotext = "RAUL";
+const meta = {
+    title: "Raul Mandujano",
+    description: "I'm Raul Mandujano, I am a full stack developer",
+};
+
+const introdata = {
+    title: "I’m Raul Mandujano",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web apps",
+    },
+    description: "I am a full stack developer with various experience projects which were performed and developed based on the needs of the client.",
+    your_img_url: "https://images.unsplash.com/photo-1668554245893-2430d0077217?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+};
+
+const dataabout = {
+    title: "About myself",
+    aboutme: "I was born in Lima Peru, I've lived in the United States for 10 years. I have a Bachelor Degree in Web Design and Development, and some graphic design certificates. I love coding and I am improving my coding skills everyday.",
+};
+const worktimeline = [{
+    jobtitle: "Front End Developer",
+    where: "Draper, Utah",
+    date: "2021",
+},
+{
+    jobtitle: "Web Developer",
+    where: "Utah",
+    date: "2021",
+},
+{
+    jobtitle: "WordPress Developer",
+    where: "Lehi, Utah",
+    date: "2020",
+},
+]
+
+const skills = [{
+        name: "HTML",
+        value: 100,
+    },
+    {
+        name: "CSS",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Django",
+        value: 50,
+    },
+    {
+        name: "MySql",
+        value: 50,
+    },
+];
+
+const services = [{
+    title: "UI & UX Design",
+    description: "I can create new and modern designs for your website. My designs are based on the trends that are used today to create a more efficient interactivity with the user.",
+},
+{
+    title: "Web Development",
+    description: "I have extensive knowledge of how to create functional web pages through different programming languages. I work through pure code or through WordPress.",
+},
+{
+    title: "Wordpress Design",
+    description: "I can create content and transform it mobile through WordPress using templates at will, or creating new templates from scratch. I implement pages according to the needs of the client/user, from start to finish.",
+},
+];
+
+const dataportfolio = [
+    {
+        img: "images/travelapp.jpg",
+        desctiption: "This project uses AirBnb Api to create a live list with price and date by date, same as the original website. I named Travel App.",
+        link: "https://meek-mandazi-606062.netlify.app/explore",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "raulmandujanoleon@gmail.com",
+    YOUR_FONE: "(385)335-3009",
+    description: "Feel free to contact me by text with any question",
+    YOUR_SERVICE_ID: "service_tlqzhid",
+    YOUR_TEMPLATE_ID: "template_sv5ydh8",
+    YOUR_USER_ID: "IHteEXMSTtQZrbwzH",
+};
+
+const socialprofils = {
+    github: "https://github.com/RaulMandujano",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/raul-mandujano-0a0858103/",
+    twitter: "https://twitter.com",
+    youtube: "https://youtube.com",
+    twitch: "https://twitch.com",
+};
+
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
