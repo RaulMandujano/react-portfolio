5caa27d6cde012e2b6ed67fe2ee450b0daa020e8
Raul Mandujano <raul@Rauls-MacBook-Pro.local>
2023-02-10 01:09:31 -0700

    portfolio update

--- src/content_option.js
+++ src/content_option.js
@@ -80,71 +80,26 @@ const dataportfolio = [
     {
         img: "images/travelapp.jpg",
         desctiption: "This project uses AirBnb Api to create a live list with price and date by date, same as the original website. I named Travel App.",
-        link: "https://meek-mandazi-606062.netlify.app/explore",
+        link: "https://meek-mandazi-606062.netlify.app/",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "images/todo-list.png",
+        desctiption: "A simple To-Do list example created with pure JS",
+        link: "https://clinquant-blancmange-5c06e4.netlify.app",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "images/svelte.png",
+        desctiption: "An example of what I can do with Svelte",
+        link: "https://nervous-stonebraker-d02ec5.netlify.app/",
     },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+    
 ];
 
 const contactConfig = {
     YOUR_EMAIL: "raulmandujanoleon@gmail.com",
     YOUR_FONE: "(385)335-3009",
     description: "Feel free to contact me by text with any question",
-    YOUR_SERVICE_ID: "service_tlqzhid",
+    YOUR_SERVICE_ID: "service_pi5160i",
     YOUR_TEMPLATE_ID: "template_sv5ydh8",
     YOUR_USER_ID: "IHteEXMSTtQZrbwzH",
 };
